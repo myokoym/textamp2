bd245f665b9babae1773b879bc2b600917fe6c20
Masafumi Yokoyama <yokoyama@clear-code.com>
2015-06-10 22:26:49 +0900

    Use select tag for linefeed

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -4,14 +4,16 @@ angular.module('textamp2')
   .controller('MainCtrl', function ($scope) {
     $scope.max_volumes = Array.apply(null, {length: 100}).map(Number.call, Number);
     $scope.volume = 10;
+    $scope.max_linefeeds = Array.apply(null, {length: 3}).map(Number.call, Number);
+    $scope.linefeed = 0;
     $scope.updateText = function () {
       var texts = []
       for (var i = 0; i < $scope.volume; i++) {
         texts.push($scope.text);
       }
-      var joinChar = '\n';
-      if ($scope.linefeed) {
-        joinChar = ('\n\n');
+      var joinChar = '';
+      for (var i = 0; i < $scope.linefeed; i++) {
+        joinChar += '\n';
       }
       $scope.amplifiedText = texts.join(joinChar);
     };
